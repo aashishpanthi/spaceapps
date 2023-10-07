1805f0f315ba7b73420574a502eaa2d0d0a50909
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-10-07 14:33:28 +0545

    information card added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TabsCustomAnimation from "../components/TabView";
+import InfoCard from "../components/Card";
 
 function Home() {
   return (
     <div>
       <div>homepage content</div>
       <TabsCustomAnimation />
+      <InfoCard />
     </div>
   );
 }
