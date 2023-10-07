2479ce5d08e12502a2b18565fe8116fe66280959
Aashish Panthi <aashishpanthi11@gmail.com>
2023-10-07 13:15:48 +0545

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-600 text-3xl"> Nasa Project !!</h1>
+      <h1 className="text-red-600 text-3xl"> Nasa Project!!</h1>
     </div>
   );
 }
