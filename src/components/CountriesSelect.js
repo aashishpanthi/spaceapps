7ac83e521001d6733c26a2f8e8d61dd6dddce10b
Aashish Panthi <aashishpanthi11@gmail.com>
2023-10-08 23:15:09 +0545

    pushed

--- src/components/CountriesSelect.js
+++ src/components/CountriesSelect.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { countryData as countries } from "../data/Countries";
 
 function CountriesSelect({ country, setCountry }) {
   return (
     <>
       <label
-        for="countries"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="countries"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
         Select a country
       </label>
       <select
         id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={country}
         onChange={(e) => setCountry(e.target.value)}
       >
-        <option value="USA" selected>
-          United States
-        </option>
+        {countries.map((country) => (
+          <option key={country.Country_Name} value={country.Country_Name}>
+            {country.Country_Name}
+          </option>
+        ))}
       </select>
     </>
   );
